96534758a240f1cdd7cdd142e665b9827a428049
Taj Gillin <tajgillin@gmail.com>
2024-10-18 23:19:27 -0400

    Only sending if in rip, not static

--- cmd/vrouter/main.go
+++ cmd/vrouter/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"ip-rip-in-peace/pkg/ipstack"
 	"os"
+	"ip-rip-in-peace/pkg/lnxconfig"
 )
 
 func main() {
@@ -22,20 +23,26 @@ func main() {
 
 	// Add handler functions
 	stack.RegisterHandler(ipstack.TEST_PROTOCOL, ipstack.PrintPacket) // Test protocol
-	stack.RegisterHandler(ipstack.RIP_PROTOCOL, ipstack.RIPHandler)   // RIP protocol
+	if stack.IPConfig.RoutingMode == lnxconfig.RoutingTypeRIP {
+		stack.RegisterHandler(ipstack.RIP_PROTOCOL, ipstack.RIPHandler)   // RIP protocol
+	}
 
 	for _, iface := range stack.Interfaces {
 		go ipstack.InterfaceListen(iface, stack)
 	}
 
-	// Send initial RIP request
-	stack.SendRIPRequest()
+	if stack.IPConfig.RoutingMode == lnxconfig.RoutingTypeRIP {
+		// Send initial RIP request
+		stack.SendRIPRequest()
 
-	// Start periodic update
+		// Start periodic update
 	go stack.PeriodicUpdate(stack.IPConfig.RipPeriodicUpdateRate)
 
-	// Start periodic timeout checking
-	go stack.RIPTimeoutCheck(stack.IPConfig.RipTimeoutThreshold)
+		// Start periodic timeout checking
+		go stack.RIPTimeoutCheck(stack.IPConfig.RipTimeoutThreshold)
+	}
+
+	
 
 	stack.Repl()
 }
